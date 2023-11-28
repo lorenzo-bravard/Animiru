c4e3f86d73d9d81383f69d6103adcd410cf3a2c7
lorenzo-bravard <103388200+lorenzo-bravard@users.noreply.github.com>
2023-11-28 10:56:01 +0100

    update version sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.animiru"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.animiru"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
