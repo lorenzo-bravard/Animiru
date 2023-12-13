c26cb8d46c77cdb7d400bb8c9c3eccefc196e7e2
cenmelt <clement.collet123@orange.fr>
2023-12-13 15:10:56 +0100

    bouton ajout fonctionnel

--- app/src/main/java/com/example/animiru/MainActivity.java
+++ app/src/main/java/com/example/animiru/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.animiru;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -21,6 +23,9 @@ import com.example.animiru.ui.ajout.AjoutFragment;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
+    private  boolean pageAjoutVisible = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,20 +38,48 @@ public class MainActivity extends AppCompatActivity {
                 .commit();
 
         // Récupération de l'ImageView à partir de la mise en page
-        ImageView imageView = findViewById(R.id.btn_ajout);
+        ImageView btn_ajout = findViewById(R.id.btn_ajout);
 
         // Ajout d'un OnClickListener à l'ImageView
-        imageView.setOnClickListener(new View.OnClickListener() {
+        btn_ajout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Appel de la méthode pour changer de fragment
-                changerDeFragment();
+                if (pageAjoutVisible == false){
+                    pageAjout();
+                    ImageView imageView = findViewById(R.id.btn_ajout);
+                    TextView textView = findViewById(R.id.titre_page);
+                    TextView biblio = findViewById(R.id.biblio);
+                    textView.setText(R.string.menu_ajout);
+                    biblio.setTextColor(getColor(R.color.gris));
+                    imageView.setImageResource(R.drawable.ann);
+                    imageView.setId(R.id.btn_ajout);
+                    pageAjoutVisible = true;
+                }else{
+                    getSupportFragmentManager().popBackStack();
+                    ImageView imageView = findViewById(R.id.btn_ajout);
+                    TextView textView = findViewById(R.id.titre_page);
+                    TextView biblio = findViewById(R.id.biblio);
+                    textView.setText(R.string.menu_biblio);
+                    biblio.setTextColor(getColor(R.color.rose));
+                    imageView.setImageResource(R.drawable.ajout);
+                    pageAjoutVisible = false;
+                }
+            }
+        });
+        TextView btn_biblio = findViewById(R.id.biblio);
+        btn_biblio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Appel de la méthode pour changer de fragment
+                pageAcceuil();
             }
         });
     }
 
     // Méthode pour changer de fragment
-    public void changerDeFragment() {
+    @SuppressLint("ResourceAsColor")
+    public void pageAjout() {
         // Création d'une instance du deuxième fragment (Fragment2)
         AjoutFragment AjoutFragment = new AjoutFragment();
 
@@ -64,10 +97,29 @@ public class MainActivity extends AppCompatActivity {
 
         // Validation de la transaction
         transaction.commit();
-        ImageView imageView = findViewById(R.id.btn_ajout);
-        TextView textView = findViewById(R.id.titre_page);
-        textView.setText(R.string.menu_ajout);
-        imageView.setImageResource(R.drawable.ann);
+    }
+    // Méthode pour changer de fragment
+    @SuppressLint("ResourceAsColor")
+    public void pageAcceuil() {
+        // Création d'une instance du deuxième fragment (Fragment2)
+        BiblioFragment BiblioFragment = new BiblioFragment();
+
+        // Obtention du gestionnaire de fragments
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // Début de la transaction de fragment
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        // Remplacement du fragment actuel par le nouveau fragment (Fragment2)
+        transaction.replace(R.id.fragment_container_view, BiblioFragment);
+
+        // Ajout à la pile de retour arrière (retour possible avec le bouton physique "Retour")
+        transaction.addToBackStack(null);
+
+        // Validation de la transaction
+        transaction.commit();
 
     }
-    }
+}
+
+
