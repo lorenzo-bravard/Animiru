cc2b8b048164a4ff9d081aa0d30b9badc2fe1f84
cenmelt <clement.collet123@orange.fr>
2023-12-13 08:56:55 +0100

    All fragments create

--- app/src/main/java/com/example/animiru/ui/ajout/AjoutFragment.java
+++ app/src/main/java/com/example/animiru/ui/ajout/AjoutFragment.java
@@ -1,4 +1,4 @@
-package com.example.animiru;
+package com.example.animiru.ui.ajout;
 
 import android.os.Bundle;
 
@@ -8,28 +8,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.animiru.databinding.FragmentBibliothequeBinding;
+import com.example.animiru.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link bibliotheque#newInstance} factory method to
+ * Use the {@link AjoutFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class bibliotheque extends Fragment {
+public class AjoutFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    private FragmentBibliothequeBinding binding;
-
-
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public bibliotheque() {
+    public AjoutFragment() {
         // Required empty public constructor
     }
 
@@ -39,11 +36,11 @@ public class bibliotheque extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment bibliotheque.
+     * @return A new instance of fragment AjoutFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static bibliotheque newInstance(String param1, String param2) {
-        bibliotheque fragment = new bibliotheque();
+    public static AjoutFragment newInstance(String param1, String param2) {
+        AjoutFragment fragment = new AjoutFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,12 +61,6 @@ public class bibliotheque extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        binding = FragmentBibliothequeBinding.inflate(inflater, container, false);
-        return binding.getRoot();
-    }
-    @Override
-    public void onViewCreated( View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        binding.anime1.setEnabled(false);
+        return inflater.inflate(R.layout.fragment_ajout, container, false);
     }
 }
